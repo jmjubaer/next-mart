39c0c650e46a457e7ece768988abdf2b7e535274
jmjubaer <jmjubaer3927@gmail.com>
2025-04-08 19:27:21 +0600

    create hero section, category and featured product section

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,11 +1,12 @@
-"use client";
 
+import Category from "@/components/modules/home/category";
 import HeroSection from "@/components/modules/home/heroSection";
 
 const HomePage = () => {
     return (
         <div>
            <HeroSection/>
+           <Category/>
         </div>
     );
 };
